5a59bb409a7db2b4cf7ce00bc52560a358a276ca
GitHub Action <action@github.com>
2020-10-15 03:51:57 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/meixuesong/idearefactoringmetrics/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.meixuesong.idearefactoringmetrics.services
+
+import com.github.meixuesong.idearefactoringmetrics.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
